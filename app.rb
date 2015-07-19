99a1b9559c1bb3da3a9bd0130865e1d111ecf8ce
Benji Dalton <daylightsavings@gmail.com>
2015-07-20 01:08:42 +1200

    adds post method for movies

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'sinatra/reloader'
 require "sinatra/json"
 
 get '/' do
-  msg = {text: 'Welcome to Movie Finder API'}
-  json msg
+  json "Welcome to Movie Finder API. POST a movie title to /movies to search."
+end
+
+post '/movies' do
+  movie = JSON.parse(request.body.read)["movie"]
+  json "Yep, I'm looking for #{movie} right now."
 end
