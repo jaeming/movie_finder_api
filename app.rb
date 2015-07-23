7a72c4ac9df54933259b9a8d6b72f46cdd99498b
Benji Dalton <daylightsavings@gmail.com>
2015-07-24 01:05:41 +1200

    adds movie search

--- app.rb
+++ app.rb
@@ -13,3 +13,14 @@ Tmdb.default_language = "en"
 get '/' do
   json "Welcome to Movie Finder API."
 end
+get '/search' do
+  query = params.keys.first.to_s
+  search_term = params[query]
+  results = []
+  movie = TmdbMovie.find("#{query}": search_term, :limit => 5)
+  movie.each do |i|
+    movie_summary = {'title' => i.title, 'description' => i.overview}
+    results << movie_summary
+  end
+  json results
+end
